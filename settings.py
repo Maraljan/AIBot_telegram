3ffb97dfaaaa8653671f02f2dc17a4cec662f312
maral <ashirova310197@gmail.com>
2020-12-12 23:38:39 +0200

    external token

--- settings.py
+++ settings.py
@@ -1,8 +1,11 @@
+import os
+
 SQL_LITE_SETTINGS = {
     'provider': 'sqlite',
     'filename': 'sqlite.db',
     'create_db': True,
 }
 
-TOKEN = '1331372142:AAFOWvY-YwvKudNAnIEti0x7RzKMWuVHPrc'
 DEFAULT_TOPIC = 'Tennis'
+
+TOKEN = os.getenv('TOKEN')
